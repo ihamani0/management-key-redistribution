14bd3c29bd0834ef803c31cc5a924402c78d51d1
hmn <hmn@gmail.com>
2025-04-10 22:33:56 +0100

    commit change

---
+++ controllers/authController.js
@@ -0,0 +1,9 @@
+
+
+export const loginController = (req , res)=>{
+    res.send('login everthing okay')
+}
+
+export const registerController = (req , res)=>{
+    res.send('register everthing okay')
+}
--- server.js
+++ server.js
@@ -1,21 +1,54 @@
-// app.js
-const express = require('express');
+// node_module
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+// You must insrte the extension if you want import file localy
+// import connectDB from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js";
+// import {errorHandler} from "./middleware/errorMiddleware.js"
+
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// 👇 Middleware to parse form data
+app.use(express.urlencoded({ extended: false }));
+
+//log
+if (process.env.NODE_ENV === "dev") {
+  app.use(morgan("dev"));
+  console.log("--- Mode : ---:", process.env.NODE_ENV);
+}
 
 // Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Example route
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js Project!');
+// Mount the authentication routes
+// app.use("/api/auth", authRoutes);
+// You could add other routes here, e.g., app.use('/api/users', userRoutes);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message || "Intrnal Server",
+    status: error.status || 500,
+  });
 });
 
+app.get("/error", (req, res, next) => {
+  const err = new Error("TUndifinde Route");
+  next(err); // This will trigger the error handler
+});
+
+// --- Error Handling Middleware ---
+// IMPORTANT: Error handler must be the LAST piece of middleware added
+
+// app.use(errorHandler);
+
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
